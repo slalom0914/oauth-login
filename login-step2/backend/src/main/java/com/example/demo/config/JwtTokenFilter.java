576738a5f2a4e7b2f194a2c7919117f388bb6272
키위 <slalom0914@gmail.com>
2026-01-16 12:07:23 +0900

    토큰발급기와 검증기 클래스완성- 3교시

--- login-step2/backend/src/main/java/com/example/demo/config/JwtTokenFilter.java
+++ login-step2/backend/src/main/java/com/example/demo/config/JwtTokenFilter.java
@@ -1,9 +1,25 @@
 package com.example.demo.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 JwtTokenFilter의 역할은 클라이언트가 요청을 할 때 토큰을 달고 다님
 이 토큰이 정상적인 것인지 서버측에서 검증하는 과정이 필요한데 이것을 여기서 처리함
@@ -14,8 +30,46 @@ public class JwtTokenFilter extends GenericFilter {
     @Value("${jwt.secret}")
     private String secretKey;
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        String token = httpRequest.getHeader("Authorization");
+        try{
+            //token이 null이라는 건 토큰을 넣지 않았다는 것임
+            if (token != null) {
+                //Bearer를 붙이는게 컨벤션이다.
+                if(!token.substring(0, 7).equals("Bearer ")) {
+                    throw new AuthenticationServiceException("Bearer 형식이 아닙니다.");
+                }
+                //검증을 할 때는 Bearer를 떼어내고 검증함.
+                String jwtToken = token.substring(7);
+                //이 토큰을 가지고 검증하고 여기서 claims는 payload를 가리키는데
+                //이것을 꺼내서 Authentication이라는 인증 객체를 만들 때 사용.
+                Claims claims = Jwts.parserBuilder()
+                        .setSigningKey(secretKey)
+                        .build()
+                        .parseClaimsJws(jwtToken)
+                        .getBody();//검증을 하고 Claims를 꺼내는 메서드임
+                List<GrantedAuthority> authorities = new ArrayList<>();//권한이 여러가지 일 수 있으므로 List에 담아줌.
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + claims.get("role")));
+                //claims.getSubject()에 Email정보가 들어있음
+                UserDetails userDetails = new User(claims.getSubject(), "", authorities);
+                //스프링에서는 Authentication객체가 있으면 로그인을 했다라고 판단함
+                Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, jwtToken, userDetails.getAuthorities());
+                //인증정보는 SecurityContextHolder안에 SecurityContext안에 들어있다.
+                //log.info(SecurityContextHolder.getContext().getAuthentication().getName());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            //아래 코드가 없으면 다음 필터로 연결이 안됨.
+            //필터를 갔다가 다시 FilterChain으로 돌아가게 하는 코드임
+            //토큰에 대한 확인이 되었으니 다시 원래 프로세스로 돌아간다.
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            httpResponse.setStatus(HttpStatus.UNAUTHORIZED.value());//401응답줌
+            httpResponse.setContentType("application/json");
+            httpResponse.getWriter().write("invalid token");
+        }
 
     }
 }
--- login-step2/backend/src/main/java/com/example/demo/config/JwtTokenProvider.java
+++ login-step2/backend/src/main/java/com/example/demo/config/JwtTokenProvider.java
@@ -1,11 +1,14 @@
 package com.example.demo.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.util.Base64;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
+import java.util.Date;
 
 /*
 JWT 토큰을 발급하는 스프링 컴포넌트(싱글톤)
@@ -23,6 +26,34 @@ public class JwtTokenProvider {
         this.secretKey = secretKey;
         this.expiration = expiration;
         this.SECRET_KEY = new SecretKeySpec(Base64.getDecoder().decode(secretKey), SignatureAlgorithm.HS512.getJcaName());
+    }//end of JwtTokenProvider
+    /*
+    Claims생성
+    JWT의 payload 부분(=실제 데이터)에 들어갈 내용
+    setSubject(email): 이 토큰의 주인은 email(주체정보)
+    claims.put("role", role): 사용자 권한(로우 하이어라키)
+    토큰 생성
+    setIssuedAt(now): 토큰 발급 시간
+    setExpiration(): 토큰 만료 시간(현재시간+유효시간)
+    signWith(SECRET_KEY):앞서 만든 Key로 HS512해시 알고리즘으로 서명
+    토큰 반환
+    .compact(): JWT문자열 직렬화해서 반환
+    이 토큰을 프론트엔드(리액트)에게 응답하면, 프론트는 이 토큰(access token)을 저장하고
+    API요청할 때 마다 Authorization헤더에 넣어 인증
+    */
+    public String createToken(String email, String role) {
+        //Claims는 jwt토큰의 payload부분을 의미함.
+        //각종 사용자 정보를 payload에 넣을 수 있다.
+        Claims claims = Jwts.claims().setSubject(email);//주된 정보는 이메일로함
+        claims.put("role", role);
+        Date now = new Date();
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now) //발행시간
+                //getTime()은 밀리세크 단위이다.
+                .setExpiration(new Date(now.getTime() + expiration*60*1000L))
+                .signWith(SECRET_KEY)
+                .compact();
+        return token;
     }
-
 }
