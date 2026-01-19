e25f1f9ebeb0dc4e887ea2b114e86e01bc28e9f0
키위 <slalom0914@gmail.com>
2026-01-19 11:54:10 +0900

    토큰연장 처리 테스트 중- 3교시

--- login-step2/frontend/src/App.jsx
+++ login-step2/frontend/src/App.jsx
@@ -1,14 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useNavigate } from "react-router-dom"
 import HomePage from "./components/pages/HomePage"
 import BoardPage from "./components/pages/BoardPage"
 import IsTokenExpiration from "./components/auth/IsTokenExpiration"
+import { useEffect, useState } from "react"
 
 function App() {
-  const token = window.localStorage.getItem('token')
+  const navigate = useNavigate()
+  //token상태를 관리하기 위해서 useState로 변경하기
+  const [token, setToken] = useState(()=>{
+    const token = window.localStorage.getItem('token')
+    return token
+  })
   //TODO - 토큰 유효시간 체크 - 파기/유지
   const isTokenExpire = IsTokenExpiration(token)
   console.log(isTokenExpire)// true이면 만료, false이면 유효
+  useEffect(() => {
+    if(isTokenExpire){
+      window.localStorage.clear()
+      navigate("/", {replace: true})
+    }
+  },[token, isTokenExpire, navigate])
   return (
     <>
       <Routes>
